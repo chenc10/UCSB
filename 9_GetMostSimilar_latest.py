89eefefc74fc47c8e7929b690ccd5b6627fbbf90
CHEN Chen <cchenam@csz338.cs.ust.hk>
2014-11-10 10:25:40 +0800

    11.10

---
+++ 9_GetMostSimilar_latest.py
@@ -0,0 +1,29 @@
+from optparse import OptionParser
+if __name__=="__main__":
+	Parser = OptionParser()
+	Parser.add_option("-r", "--read", dest="readfilename")
+	(options, args) = Parser.parse_args()
+	sReadFileName=options.readfilename
+	hFread = open(sReadFileName,'r')
+	sDatabase = hFread.readlines()
+	hFread.close()
+	fMaxSimilarity = 0.000000
+	lList = []
+	nMaxRow = 0
+	i = 0
+	while i < len(sDatabase):
+		print sDatabase[i][0:6]
+		if sDatabase[i][0:6] == "result":
+			print i
+			print sDatabase[i][7:]
+			fTmp = float(sDatabase[i][7:])
+			lList.append(fTmp)
+			if fTmp > fMaxSimilarity:
+				fMaxSimilarity = fTmp
+				nMaxRow = i
+		i = i + 1
+	lList.sort()
+	print "MaxSimilarity: ",fMaxSimilarity
+	print "Row ",nMaxRow
+	print lList[-10:]
+	print sum(lList)/len(lList)
